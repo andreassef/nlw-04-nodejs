2d0af5a10fb77efa187c14e57c82573ca1eda5bc
andreassef <andreassef02@gmail.com>
2021-02-25 12:29:47 -0400

    [ADD] - Novas rotas, uma para criar um survey e outra para listar

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import SurveyController from './controllers/SurveyController';
 import UserController from './controllers/UserController';
 
 const userController = new UserController();
+const surveyController = new SurveyController();
 
 const router = Router();
 
+router.get("/surveys", surveyController.show);
 router.post("/users", userController.create);
+router.post("/surveys", surveyController.create);
 
 export default router;

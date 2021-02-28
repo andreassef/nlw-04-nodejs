1a16d63156d7507cd9c3438449bce187ed89988c
andreassef <andreassef02@gmail.com>
2021-02-27 21:00:11 -0400

    [NEW] - Nova rota para envio de email

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
+import SendMailController from './controllers/SendMailController';
 import SurveyController from './controllers/SurveyController';
 import UserController from './controllers/UserController';
 
 const userController = new UserController();
 const surveyController = new SurveyController();
-
+const sendMailController = new SendMailController();
 const router = Router();
 
 router.get("/surveys", surveyController.show);
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
+router.post("/sendMail", sendMailController.excute);
 
 export default router;

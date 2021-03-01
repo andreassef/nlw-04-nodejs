4fc918153f9f873517c9485441f18ec66c04609b
andreassef <andreassef02@gmail.com>
2021-02-28 23:23:23 -0400

    [ADD] - Adicao de validacoes de campos

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,32 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import UsersRepository from "../repositories/UsersRepository";
+import * as yup from 'yup';
+import AppError from "../errors/AppError";
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().max(10).required(),
+            email: yup.string().email().required()
+        });
+
+        // if (!(await schema.isValid(request.body))) {
+        //     return response.status(400).json({error: "Validation Failed"});
+        // }
+
+        try {
+            await schema.validate(request.body);
+        } catch (error) {
+            if (error.type == 'max') {
+                throw new AppError("O campo nome pode ter no máximo 10 caracteres!");
+                //return response.status(400).json({error: "O campo nome pode ter no máximo 10 caracteres!"})
+            }
+            return response.status(400).json({error: error})
+        }
 
         const userRepository = getCustomRepository(UsersRepository)
 
@@ -14,7 +35,8 @@ class UserController {
         })
 
         if(userAlreadyExists) {
-            return response.status(400).json({error: "Usuário já cadastrado"})
+            throw new AppError("Usuário já cadastrado");
+            //return response.status(400).json({error: "Usuário já cadastrado"})
         }
 
         const user = userRepository.create({

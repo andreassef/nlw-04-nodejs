83820952ab605dea10ac3322ef1d4d29479661b1
andreassef <andreassef02@gmail.com>
2021-02-28 23:28:04 -0400

    [ADD] - Adicao de novas rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import AnswerController from './controllers/AnswerController';
+import NpsController from './controllers/NpsController';
 import SendMailController from './controllers/SendMailController';
 import SurveyController from './controllers/SurveyController';
 import UserController from './controllers/UserController';
@@ -6,9 +8,15 @@ import UserController from './controllers/UserController';
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
+
 const router = Router();
 
 router.get("/surveys", surveyController.show);
+router.get("/answers/:value", answerController.execute);
+router.get("/nps/:survey_id", npsController.execute);
+
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
 router.post("/sendMail", sendMailController.excute);

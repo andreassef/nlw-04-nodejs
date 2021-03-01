8f915cd9c3b8daa8d792e9ce5c53481b955880ba
andreassef <andreassef02@gmail.com>
2021-02-28 23:22:13 -0400

    [NEW] - Novo modulo para tratamento de erro

---
+++ src/errors/AppError.ts
@@ -0,0 +1,12 @@
+
+class AppError {
+    public readonly message: string;
+    statusCode: number;
+
+    constructor(message: string, statusCode = 400) {
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+export default AppError;

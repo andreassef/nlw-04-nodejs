f756e281b6c5f1e935743a647d44280d9aac49b9
andreassef <andreassef02@gmail.com>
2021-02-28 23:25:26 -0400

    [UPDATE] - Ajuste em bugs e tratamento de erros

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -5,6 +5,7 @@ import SurveyUsersRepository from "../repositories/SurveysUsersRepository";
 import UsersRepository from "../repositories/UsersRepository";
 import SendMailService from "../services/SendMailService";
 import { resolve } from 'path';
+import AppError from "../errors/AppError";
 
 
 class SendMailController {
@@ -19,32 +20,37 @@ class SendMailController {
         const userAlreadExists = await usersRepository.findOne({email});
 
         if (!userAlreadExists) {
-            return response.status(400).json({error: "Users does not exists"});
+            throw new AppError("Users does not exists");
+            //return response.status(400).json({error: "Users does not exists"});
         };
 
         const surveyAlreadExists = await surveyRepository.findOne({id: survey_id});
 
         if (!surveyAlreadExists) {
-            return response.status(400).json({
-                error: "Survey does not exists!"
-            })
+            throw new AppError("Survey does not exists!");
+            // return response.status(400).json({
+            //     error: "Survey does not exists!"
+            // })
         }
 
         const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+        const surveyUserAlreadyExists = await surveyUsersRepository.findOne({
+            //where: [{user_id: userAlreadExists.id}, {value: null}] -> dessa forma, estavamos utilizando o operador OR, de modo que nossa consulta sempre iria retornar o primeiro usuário com ID e valor NULL que encontrasse
+            where: {user_id: userAlreadExists.id, value: null}, // dessa forma, estamos fazendo a consulta com AND, onde o id do usuário é igual e valor null
+            relations: ["user", "survey"]
+        });
+
         const variables = {
             name: userAlreadExists.name,
             title: surveyAlreadExists.title,
             description: surveyAlreadExists.description,
-            user_id: userAlreadExists.id,
+            id: "",
             link: process.env.URL_MAIL
         }
 
-        const surveyUserAlreadyExists = await surveyUsersRepository.findOne({
-            where: [{user_id: userAlreadExists.id}, {value: null}],
-            relations: ["user", "survey"]
-        });
-
         if (surveyUserAlreadyExists) {
+            variables.id = surveyUserAlreadyExists.id;
             await SendMailService.execute(email, surveyAlreadExists.title, variables, npsPath);
             return response.json(surveyUserAlreadyExists);
         }
@@ -58,6 +64,7 @@ class SendMailController {
         await surveyUsersRepository.save(surveyUser);
 
         // Enviar e-mail para o usuário
+        variables.id = surveyUserAlreadyExists.id;
         await SendMailService.execute(email, surveyAlreadExists.title, variables, npsPath);
 
         return response.status(201).json(surveyUser);
